e888372cb576d7937e957e42cbf3b387dadacc54
Aliaksei Slepau <aslepaugo@aleksc-mac.local>
2020-10-18 09:27:52 +0300

    Added sqlalchemy and sqlite

--- webapp/config.py
+++ webapp/config.py
@@ -1,1 +1,6 @@
-WEATHER_DEFAULT_CITY="Wroclaw"
+import os
+
+basedir = os.path.abspath(os.path.dirname(__filename__))
+
+WEATHER_DEFAULT_CITY = "Wroclaw"
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'wbeapp.db')
