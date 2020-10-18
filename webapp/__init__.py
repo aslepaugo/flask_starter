bdee0efd9c025390d4c32a42bb4d9272aa6ab36b
Aliaksei Slepau <aslepaugo@aleksc-mac.local>
2020-10-18 11:10:14 +0300

    Save news to the DB

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template
 
-from webapp.python_org_news import get_python_news
 from webapp.weather import weather_by_city
-from webapp.model import db
+from webapp.model import db, News
 
 def create_app():
     app = Flask(__name__)
@@ -13,7 +12,7 @@ def create_app():
     def index():
         page_title = "Python news"
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
-        news_list = get_python_news()
+        news_list = News.query.order_by(News.published.desc()).all()
 
         return render_template('index.html', page_title=page_title, weather=weather, news_list=news_list)
 
